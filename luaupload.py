a33834c588a190b663757364520dd0e71b8315bf
Liam Marshall <archimedespi3141@gmail.com>
2014-11-25 17:30:11 -0600

    Untested, unsecure Telnet functionality

--- luaupload.py
+++ luaupload.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import time
+import socket
 
 import click
 import serial
@@ -11,6 +12,53 @@ import serial
 @click.group()
 def cli():
 	pass
+
+def telnetupload(url, file):
+
+	try:
+		f = open(file, 'r')
+	except:
+		click.echo("Could not open file {file}".format(file=file))
+		raise
+
+	ip, seperator, port = url.rpartition(':')
+	if not seperator:
+		port = 3232
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	except:
+		click.echo("Could not connect to socket")
+		raise
+
+	s.connect((ip, port))
+
+	click.echo("Starting download to module...")
+	s.send('file.remove("{file}")\n'.format(file=filename))
+	click.echo('file.remove("{file}")'.format(file=filename))
+	time.sleep(0.1)
+
+	s.send('file.open("{file}", "w")\n'.format(file=filename))
+	click.echo('file.open("{file}", "w")'.format(file=filename))
+	s.send('file.writeline([[print(1)]])\n')
+	click.echo('file.writeline([[print(1)]])')
+	s.send('file.close()\n')
+	click.echo('file.close()')
+	time.sleep(0.1)
+
+	s.send('file.open("{file}", "w+")\n'.format(file=filename))
+	click.echo('file.open("{file}", "w+")'.format(file=filename))
+	time.sleep(0.1)
+
+	for line in f:
+		s.send('file.writeline([[{line}]])\n'.format(line=line.strip()))
+		click.echo('file.writeline([[{line}]])\n'.format(line=line.strip()))
+		time.sleep(0.1)
+
+	s.send('file.close()\n')
+
+	s.close()
+	f.close()
+
 
 @cli.command()
 @click.argument('file')
@@ -34,7 +82,7 @@ def run(file, port, baud):
 		click.echo(line)
 		time.sleep(0.1)
 
-	s.write(line+'\r\n')
+	s.write(line+'\n')
 
 @cli.command()
 @click.argument('file')
@@ -56,29 +104,29 @@ def upload(file, port, baud):
 		raise
 
 	click.echo("Starting download to module...")
-	s.write('file.remove("{file}")\r\n'.format(file=filename))
+	s.write('file.remove("{file}")\n'.format(file=filename))
 	click.echo('file.remove("{file}")'.format(file=filename))
 	time.sleep(0.1)
 
-	s.write('file.open("{file}", "w")\r\n'.format(file=filename))
+	s.write('file.open("{file}", "w")\n'.format(file=filename))
 	click.echo('file.open("{file}", "w")'.format(file=filename))
-	s.write('file.writeline([[print(1)]])\r\n')
+	s.write('file.writeline([[print(1)]])\n')
 	click.echo('file.writeline([[print(1)]])')
-	s.write('file.close()\r\n')
+	s.write('file.close()\n')
 	click.echo('file.close()')
 	time.sleep(0.1)
 
-	s.write('file.open("{file}", "w+")\r\n'.format(file=filename))
+	s.write('file.open("{file}", "w+")\n'.format(file=filename))
 	click.echo('file.open("{file}", "w+")'.format(file=filename))
 	time.sleep(0.1)
 
 	for line in f:
 		if line.strip() != "":
-			s.write('file.writeline([[{line}]])\r\n'.format(line=line.strip()))
+			s.write('file.writeline([[{line}]])\n'.format(line=line.strip()))
 			click.echo('file.writeline([[{line}]])'.format(line=line.strip()))
 			time.sleep(0.25)
 
-	s.write('file.close()\r\n')
+	s.write('file.close()\n')
 	click.echo('file.close()')
 
 	s.close()
