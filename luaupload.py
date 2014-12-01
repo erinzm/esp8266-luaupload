e45e7350ddbb3074c31a20c81b0c09fff52af3a3
Liam Marshall <archimedespi3141@gmail.com>
2014-11-30 18:52:29 -0600

    Transition serial_upload to new API

--- luaupload.py
+++ luaupload.py
@@ -113,42 +113,8 @@ def upload(file, port, baud):
 		click.echo("Could not open file {file}".format(file=file))
 		raise
 
-	try:
-		s = serial.Serial(port, baud)
-	except:
-		click.echo("Could not open {port} at {baud}".format(port=port, baud=baud))
-		raise
-
-	click.echo("Starting download to module...")
-	s.write('file.remove("{file}")\n'.format(file=filename))
-	click.echo('file.remove("{file}")'.format(file=filename))
-	time.sleep(0.1)
-
-	s.write('file.open("{file}", "w")\n'.format(file=filename))
-	click.echo('file.open("{file}", "w")'.format(file=filename))
-	s.write('file.writeline([[print(1)]])\n')
-	click.echo('file.writeline([[print(1)]])')
-	s.write('file.close()\n')
-	click.echo('file.close()')
-	time.sleep(0.1)
-
-	s.write('file.open("{file}", "w+")\n'.format(file=filename))
-	click.echo('file.open("{file}", "w+")'.format(file=filename))
-	time.sleep(0.1)
-
-	for line in f:
-		if line.strip() != "":
-			s.write('file.writeline([[{line}]])\n'.format(line=line.strip()))
-			click.echo('file.writeline([[{line}]])'.format(line=line.strip()))
-			time.sleep(0.25)
-
-	s.write('file.close()\n')
-	click.echo('file.close()')
-
-	s.close()
-	f.close()
-
-	click.echo("Done downloading to module.")
+	esp.upload(f.readlines(), {'filename': filename},
+		'serial', {'port': port, 'baud': baud})
 
 if __name__ == '__main__':
 	cli()
